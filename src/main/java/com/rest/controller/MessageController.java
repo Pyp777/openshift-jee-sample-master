2030e1de4176e48d86d8acb29d470d4a311aec81
pavel <pavel@pavel>
2019-04-06 08:50:41 +0200

    ajax

---
+++ src/main/java/com/rest/controller/MessageController.java
@@ -0,0 +1,45 @@
+package com.rest.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/message")
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
+public class MessageController {
+ 
+    @GET
+    @Path("/status")
+    public String getStatus() {
+    	return "Random service is running";
+    }
+
+    @POST
+    @Path("/clear")
+    public String clearStatus() {
+    	return "";
+    }
+    
+    /**
+     * call jms!
+     * @return
+     */
+    @POST
+    @Path("/send/{message}")
+    public String sendMessage(@PathParam("message") String message) {
+    	return message + " is accepted...";
+    }
+    
+    /**
+     * read jms!
+     * @return
+     */
+    public String getMessage() {
+    	return "";
+    }
+    
+}
